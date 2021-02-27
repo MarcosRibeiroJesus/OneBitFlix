0de69ed62e2c598a40e49178d60fa24e080005e7
Marcos Ribeiro <mrj84@outlook.com>
2021-02-27 17:58:29 -0300

    fix(last_watched_migration): fix typo
    Signed-off-by: Marcos Ribeiro <mrj84@outlook.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210227203140) do
+ActiveRecord::Schema.define(version: 20210227204422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "pg_trgm"
   enable_extension "fuzzystrmatch"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.string   "favoritable_type"
+    t.integer  "favoritable_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["favoritable_type", "favoritable_id"], name: "index_favorites_on_favoritable_type_and_favoritable_id", using: :btree
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "thumbnail_key"
+    t.string   "video_key"
+    t.boolean  "highlighted",   default: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+  end
 
   create_table "pg_search_documents", force: :cascade do |t|
     t.text     "content"
@@ -24,6 +48,24 @@ ActiveRecord::Schema.define(version: 20210227203140) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "series", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "thumbnail_key"
+    t.boolean  "highlighted",             default: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
+    t.integer  "last_watched_episode_id"
+    t.index ["last_watched_episode_id"], name: "index_series_on_last_watched_episode_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +86,5 @@ ActiveRecord::Schema.define(version: 20210227203140) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "series", "movies", column: "last_watched_episode_id"
 end
