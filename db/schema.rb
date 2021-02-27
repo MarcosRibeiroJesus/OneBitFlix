525fe49ad0988f2a523a03471eafce189f52674b
Marcos Ribeiro <mrj84@outlook.com>
2021-02-27 19:45:08 -0300

    feat(concerns): highlightable special validation
    Signed-off-by: Marcos Ribeiro <mrj84@outlook.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210227204422) do
+ActiveRecord::Schema.define(version: 20210227221117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,18 @@ ActiveRecord::Schema.define(version: 20210227204422) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "description"
+    t.string   "reviewable_type"
+    t.integer  "reviewable_id"
+    t.integer  "user_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["reviewable_type", "reviewable_id"], name: "index_reviews_on_reviewable_type_and_reviewable_id", using: :btree
+    t.index ["user_id"], name: "index_reviews_on_user_id", using: :btree
+  end
+
   create_table "series", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
@@ -86,5 +98,6 @@ ActiveRecord::Schema.define(version: 20210227204422) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "reviews", "users"
   add_foreign_key "series", "movies", column: "last_watched_episode_id"
 end
